1b52ac449610b58ff1076ad2db88a84035cf9885
Victor <133044234+ViWhite1@users.noreply.github.com>
2023-07-05 10:33:48 +0300

    Add files via upload

---
+++ shelving-boxes.php
@@ -0,0 +1,51 @@
+<?php
+$shelving_len = 1310;
+$shelving = array(1 => array(), 2 => array(), 3 => array(), 4 => array());
+$boxes = [1 => 774, 2 => 214, 3 => 694, 4 => 321, 5 => 674, 6 => 527, 7 => 120, 8 => 567];
+$shelf = 1;
+arsort($boxes);
+
+while (empty($boxes)==false) {
+    if (current($boxes) == null) {
+        reset($boxes);
+    }
+    elseif (current($boxes) + array_sum($shelving[$shelf]) <= $shelving_len)  {
+        empty($shelving[$shelf]) ?
+            $shelving[$shelf] = array(key($boxes) => current($boxes)) :
+            $shelving[$shelf] += array(key($boxes) => current($boxes));
+        unset($boxes[key($boxes)]);
+    }
+    elseif (min($boxes)+array_sum($shelving[$shelf]) <= $shelving_len) {
+        next($boxes);
+    }
+    else {
+        $shelving[$shelf] += array('empty' =>  $shelving_len-array_sum($shelving[$shelf]));
+        $shelf += 1;
+    }
+}
+$shelving[$shelf] += array('empty' =>  $shelving_len-array_sum($shelving[$shelf]));
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<table cellspacing="4" cellpadding="4" border="1">
+    <?php foreach ($shelving as $shelf => $boxes) {?>
+        <tr>
+            <td bgcolor="#b0c4de"> Полка <?php echo $shelf; foreach ($boxes as $num => $wid) {?> </td>
+            <div style="width: <?php echo $wid; ?>px"><td><?php if ($num!='empty') { echo 'Коробка '.$num;} else {echo $num;} ?></td> </div>
+            <?php }?>
+        </tr>
+    <?php }?>
+</table>
+</body>
+</html>
+
+
+
